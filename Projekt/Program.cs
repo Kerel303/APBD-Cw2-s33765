802790649884d803bdc20723d7f9fcd37c3fe4f2
Karol Foltyn <s33765@pjwstk.edu.pl>
2026-03-20 19:51:29 +0100

    Added Repositories as databases for the project

--- Projekt/Program.cs
+++ Projekt/Program.cs
@@ -4,87 +4,41 @@ using System.Text.Json;
 
 public class Program
 {
-    // Listy
-    static public List<User> Users = new List<User>();
-    static public List<Due> Dues = new List<Due>();
-    static public List<Equipment> Equipments = new List<Equipment>();
-    static public List<Lease> Leases = new List<Lease>(); 
-    
+    // Repozytoria
+    static Repository<User> Users;
+    static Repository<Due> Dues;
+    static Repository<Equipment> Equipments;
+    static Repository<Lease> Leases;
     
     static void Main(String[] args)
     {
-        loadLists();
-        Console.WriteLine("Start of the program.");
+        string basePath = AppDomain.CurrentDomain.BaseDirectory;
+        
+        Users = new Repository<User>(GetPath("users.json"));
+        Dues = new Repository<Due>(GetPath("dues.json"));
+        Equipments = new Repository<Equipment>(GetPath("equipments.json"));
+        Leases = new Repository<Lease>(GetPath("leases.json"));
+        
+        Console.WriteLine("Database Loaded");
+        
     }
+
+
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    // Wczytywanie List
-    private static void loadLists()
+    // Wyświetlanie całej listy (Trzeba dodać do każdej głównej klasy ovveride na String)
+    public static void printList<T>(List<T> list)
     {
-        string users = GetPath("users.json");
-        string dues = GetPath("dues.json");
-        string equipments = GetPath("equipments.json");
-        string leases = GetPath("leases.json");
-        
-        EnsureFile(users);
-        EnsureFile(dues);
-        EnsureFile(equipments);
-        EnsureFile(leases);
-        
-        Users = JsonSerializer.Deserialize<List<User>>(File.ReadAllText(users)) ?? new List<User>();
-        Dues = JsonSerializer.Deserialize<List<Due>>(File.ReadAllText(dues)) ?? new List<Due>();
-        Equipments = JsonSerializer.Deserialize<List<Equipment>>(File.ReadAllText(equipments)) ?? new List<Equipment>();
-        Leases = JsonSerializer.Deserialize<List<Lease>>(File.ReadAllText(leases)) ?? new List<Lease>();
+        foreach (T item in list)
+        {
+            Console.WriteLine(item);
+        }
     }
     
-    // Zapisywanie List
-    private static void saveLists()
-    {
-        string users = GetPath("users.json");
-        string dues = GetPath("dues.json");
-        string equipments = GetPath("equipments.json");
-        string leases = GetPath("leases.json");
-        
-        EnsureFile(users);
-        EnsureFile(dues);
-        EnsureFile(equipments);
-        EnsureFile(leases);
-        
-        File.WriteAllText(users, JsonSerializer.Serialize(Users));
-        File.WriteAllText(dues, JsonSerializer.Serialize(Dues));
-        File.WriteAllText(equipments, JsonSerializer.Serialize(Equipments));
-        File.WriteAllText(leases, JsonSerializer.Serialize(Leases));
-    }
     // Pobieranie ścieżki
     private static string GetPath(string file)
     {
         return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, file);
-    }
-
-
-    // Tworzenie brakujących plików
-    private static void EnsureFile(string path)
-    {
-        if (!File.Exists(path))
-        {
-            File.WriteAllText(path, "[]"); // pusta lista JSON
-        }
     }
 
 }
