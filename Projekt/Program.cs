2bb99afa18056ad4dc48fb29453374ac1a07c4bd
Karol Foltyn <s33765@pjwstk.edu.pl>
2026-03-22 14:24:46 +0100

    Work in progress

--- Projekt/Program.cs
+++ Projekt/Program.cs
@@ -25,6 +25,7 @@ public class Program
 
     private static void StartProgram()
     {
+        Console.WriteLine("Program Started");
         while (isRunning)
         {
             Console.Write("> ");
@@ -32,6 +33,7 @@ public class Program
             
             string[] inputArgs = input.Split(' ');
             string command = inputArgs[0];
+            int args =  inputArgs.Length - 1;
 
             switch (command)
             {
@@ -39,7 +41,76 @@ public class Program
                     isRunning = false;
                     break;
                 case "help":
-                    Console.WriteLine("Here will be listed all availible commands");
+                    Console.WriteLine("Available commands:\n\n" +
+                                      "help - Shows available commands.\n\n" +
+                                      "add - Adds the type you want to the database\n" +
+                                      "Like: user, lease, equipment, due\n\n" +
+                                      "list - Makes a list of objects currently in the database.\n" +
+                                      "Example: list users\n" +
+                                      "You can choose from: equipments, leases, dues, users, all\n\n");
+                    break;
+                case "list":
+                    if (args == 1)
+                    {
+                        if (inputArgs[1] == "equipments")
+                        {
+                            PrintList(Equipments.GetList());
+                        }else if (inputArgs[1] == "leases")
+                        {
+                            PrintList(Leases.GetList());    
+                        }else if (inputArgs[1] == "dues")
+                        {
+                            PrintList(Dues.GetList());
+                        }else if (inputArgs[1] == "users")
+                        {
+                            PrintList(Users.GetList());
+                        }else if (inputArgs[1] == "all")
+                        {
+                            Console.WriteLine("Users: ");
+                            PrintList(Users.GetList());
+                            Console.WriteLine("Equipments: ");
+                            PrintList(Equipments.GetList());
+                            Console.WriteLine("Leases: ");
+                            PrintList(Leases.GetList());
+                            Console.WriteLine("Dues: ");
+                            PrintList(Dues.GetList());
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Proper use of the command: list <nameTolist>");
+                        Console.WriteLine("Names to list: equipments, leases, dues, users, all");
+                    }
+                    break;
+                case "add":
+                    if (args > 1)
+                    {
+                        if (inputArgs[1] == "equipment")
+                        {
+
+                        }else if (inputArgs[1] == "lease")
+                        {
+                            
+                        }else if (inputArgs[1] == "user")
+                        {
+                            
+                        }else if (inputArgs[1] == "due")
+                        {
+                            
+                        }else
+                        {
+                            Console.WriteLine("Proper use of the command: add <nameToAdd>");
+                            Console.WriteLine("Names to add: equipment, lease, due, user");
+                            Console.WriteLine("The rest of the command depends on the type you selected.");
+                        }
+                        
+                    }else
+                    {
+                        Console.WriteLine("Proper use of the command: add <nameToAdd>");
+                        Console.WriteLine("Names to add: equipment, lease, due, user");
+                        Console.WriteLine("The rest of the command depends on the type you selected.");
+                    }
+
                     break;
                 default:
                     Console.WriteLine($"'{input}' is not recognized as an available command");
